4bbce6633cb716494dc8dc7ec0e8da6bbd48c8c9
origin <chldlsrb1000@naver.com>
2021-04-07 11:26:43 +0900

    구글, 네이버 로그인 구현 (로그인시 리다이렉트 확인)

--- src/main/java/org/me/springboot/web/IndexController.java
+++ src/main/java/org/me/springboot/web/IndexController.java
@@ -1,12 +1,16 @@
 package org.me.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.me.springboot.config.auth.LoginUser;
+import org.me.springboot.config.auth.dto.SessionUser;
 import org.me.springboot.service.posts.PostsService;
 import org.me.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,8 +19,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
