bd973d15b0049243ea842f5bb902bc73e51c1acb
origin <chldlsrb1000@naver.com>
2021-04-13 13:45:14 +0900

    테스트 Apicontroller +CrossOrigin

--- src/main/java/org/me/springboot/web/TestApiController.java
+++ src/main/java/org/me/springboot/web/TestApiController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.me.springboot.service.posts.TestService;
 import org.me.springboot.web.dto.testApi.TestApiRequestDto;
 import org.me.springboot.web.dto.testApi.TestApiResponseDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,7 +14,8 @@ public class TestApiController {
 
     private final TestService testService;
 
-    @PostMapping("/test/api/main/number")
+    @CrossOrigin("*")
+    @RequestMapping("/test/api/main/number")
     public TestApiResponseDto apiTakeAndGive(@RequestBody TestApiRequestDto requestDto) {
         return testService.FourRuleCalculations(requestDto);
     }
