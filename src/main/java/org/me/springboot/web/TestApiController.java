e8ce1e1ad200c0fed0449b359ab33bd35e21caeb
origin <chldlsrb1000@naver.com>
2021-04-13 12:00:27 +0900

    테스트 Apicontroller Test

--- src/main/java/org/me/springboot/web/TestApiController.java
+++ src/main/java/org/me/springboot/web/TestApiController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RequiredArgsConstructor
 @RestController
 public class TestApiController {
@@ -15,7 +17,9 @@ public class TestApiController {
     private final TestService testService;
 
     @PostMapping("/test/api/main/number")
-    public TestApiResponseDto apiTakeAndGive(@RequestBody TestApiRequestDto requestDto) {
+    public TestApiResponseDto apiTakeAndGive(@RequestBody TestApiRequestDto requestDto, HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "origin, x-requested-with, content-type, accept");
         return testService.FourRuleCalculations(requestDto);
     }
 }
